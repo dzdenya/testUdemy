49b2fd88f18e243dd7f6421e95f423baec4adfac
Denys Zverev <dzdenya@gmail.com>
2018-04-29 15:06:54 +0300

    4. Запуск браузера и первые действия(Урок 40)

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -5,13 +6,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class MainClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\Denys\\IdeaProjects\\testUdemy\\drivers\\geckodriver.exe");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Denys\\IdeaProjects\\testUdemy\\drivers\\chromedriver.exe");
 
         WebDriver driver = new FirefoxDriver();
-        WebDriver driver1 = new ChromeDriver();
 
-        driver.get("http://www.google.com");
-        driver1.get("http://www.google.com");
+        driver.manage().window().setSize(new Dimension(1600,500));
+
+        driver.get("http://google.com");
+        driver.get("http://seleniumhq.org");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
 
 
     }
