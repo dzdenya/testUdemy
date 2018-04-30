2b7b7b55d50804b7e7a1613189ed4d51dac33a72
Denys Zverev <dzdenya@gmail.com>
2018-04-30 23:37:19 +0300

    7. Работа с текстовыми полями(урок 44)

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -16,17 +16,26 @@ public class MainClass {
         driver.manage().window().maximize();
 
         driver.get("http://en.wikipedia.org");
+        driver.findElement(By.xpath("//input[@id='searchInput']"))
+                .sendKeys("Selenium WebDriver");
         driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+        System.out.println(driver
+                .findElement(By.xpath("//div[@id='searchText']/input")).getAttribute("value"));
+        driver.findElement(By.xpath("//div[@id='searchText']/input")).clear();
 
         driver.get("https://github.com/");
+        driver.findElement(By.xpath(".//*[@id='user[login]']")).sendKeys("test@gmail.com");
+        driver.findElement(By.xpath(".//*[@id='user[password]']")).sendKeys("test7654321");
         WebElement button = driver.findElement(By.xpath("//form[@class='home-hero-signup js-signup-form']//button"));
         if (button.getText().equals("Sign up for GitHub")){
             System.out.println("Success");
         }else System.out.println("Fail");
-//        button.submit();
+        button.submit();
         driver.findElement(By.xpath("//a[text()='Sign in']")).click();
 
         driver.get("https://facebook.com/");
+        driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("test@gmail.com");
+        driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("test7654321");
         driver.findElement(By.xpath("//label[@id='loginbutton']/input")).submit();
 
 
