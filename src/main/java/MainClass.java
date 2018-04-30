bb188d5cd84d0e6e6c4fe070ad3774c55cb537c4
Denys Zverev <dzdenya@gmail.com>
2018-04-30 21:58:26 +0300

    5. Поиск элементов(урок 42)

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -9,17 +12,20 @@ public class MainClass {
 
         WebDriver driver = new FirefoxDriver();
 
-        driver.manage().window().setSize(new Dimension(1600,500));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
-        driver.get("http://google.com");
-        driver.get("http://seleniumhq.org");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        driver.get("http://en.wikipedia.org");
+
+        WebElement link = driver.findElement(By.linkText("Log in"));
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+        WebElement link3 = driver.findElement(By.name("search"));
+        WebElement link4 = driver.findElement(By.className("searchButton"));
+        WebElement link5 = driver.findElement(By.id("ca-viewsource"));
+        WebElement link6 = driver.findElement(By.tagName("input"));
+        WebElement link7 = driver.findElement(By.cssSelector("div#simpleSearch input#searchButton"));
+        WebElement link8 = driver.findElement(By.xpath("//div[@id='mw-panel']/div[@id='p-logo']//a"));
+
         driver.quit();
-
-
     }
 }
