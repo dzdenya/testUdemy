ad2c1cdd9092e912a4b05b694580e0346d14cfce
Denys Zverev <dzdenya@gmail.com>
2018-05-03 14:10:09 +0300

    10. Поиск списка элементов и выделение всех чекбоксов

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,11 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
@@ -23,17 +21,15 @@ public class MainClass {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //        driver.manage().window().maximize();
 
-        driver.get("https://www.facebook.com/?stype=lo&jlou=Affqexabs4O44j2LDKMMsugQ5m_zE6e_eMrgLfZU8kqkM4bXFcx1GcvznCYB8rCyZI8Xmz-x2McWvsKoHXYjnreIDT6lklXVUrhJ-45HicniMQ&smuh=32118&lh=Ac9ItPFHrHy_Ek-p");
+        driver.get("https://www.moyo.ua/telecommunication/smart/apple/");
 
-        selectOption("day","5");
-        selectOption("month","8");
-        selectOption("year","5");
 
-    }
-    public static void selectOption(String listName, String option){
-        String listXpath = String.format("//select[@id='%s']", listName);
-        String optionXpath = String.format("//select[@id='%s']/option[%s]", listName, option);
-        driver.findElement(By.xpath(listXpath)).click();
-        driver.findElement(By.xpath(optionXpath)).click();
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//div[@class='brands_filter']//div[@class='filter_item']"));
+        if (checkboxes.size() == 20) System.out.println("okay");
+        else System.out.println("fail");
+
+        for (WebElement checkbox:checkboxes) {
+            checkbox.click();
+        }
     }
 }
