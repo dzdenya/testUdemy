92a6a75532f466d8929b3386076c3d3ab96a89b8
Denys Zverev <dzdenya@gmail.com>
2018-05-04 13:38:12 +0300

    11. Сложные действия(dragAndDrop)

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,9 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,19 +19,36 @@ public class MainClass {
 
         driver = new ChromeDriver();
 
-        wait = new WebDriverWait(driver, 10);
+//        wait = new WebDriverWait(driver, 10);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().window().maximize();
+        driver.manage().window().maximize();
 
-        driver.get("https://www.moyo.ua/telecommunication/smart/apple/");
+        driver.get("https://jqueryui.com/droppable/#accepted-elements");
+        WebElement fr = driver.findElement(By.xpath("//iframe"));
+        driver.switchTo().frame(fr);
+        WebElement link = driver.findElement(By.xpath("//div[@id='draggable-nonvalid']"));
+        WebElement link2 = driver.findElement(By.xpath("//div[@id='draggable']"));
+        WebElement link3 = driver.findElement(By.xpath("//div[@id='droppable']"));
+//
+//        driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2");
+//          driver.switchTo().frame("IF1");
+//        WebElement link = driver.findElement(By.xpath("//div[@id='div1']"));
+////        WebElement link1 = driver.findElement(By.xpath("//div[@id='draggable']"));
+//        WebElement link2 = driver.findElement(By.xpath("//div[@id='div2']"));
+//
+        Actions actions = new Actions(driver);
+////        actions.moveToElement(link).build().perform();
+//
+//        actions.dragAndDrop(link, link3).perform();
+        actions.dragAndDrop(link2, link3).perform();
+////
+        actions.clickAndHold(link).moveToElement(link3).release().build().perform();
 
-
-        List<WebElement> checkboxes = driver.findElements(By.xpath("//div[@class='brands_filter']//div[@class='filter_item']"));
-        if (checkboxes.size() == 20) System.out.println("okay");
-        else System.out.println("fail");
-
-        for (WebElement checkbox:checkboxes) {
-            checkbox.click();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        driver.quit();
     }
 }
---
+++ src/main/java/Table.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Table {
+    private WebElement tableElement;
+    private WebDriver driver;
+
+    public Table(WebElement tableElement, WebDriver driver) {
+        this.tableElement = tableElement;
+        this.driver = driver;
+    }
+
+    public List<WebElement> getRows() {
+        List<WebElement> rows = tableElement.findElements(By.xpath(".//tr"));
+        rows.remove(0);
+        return rows;
+    }
+
+    public List<WebElement> getHeadings() {
+        WebElement headingsRow = tableElement.findElement(By.xpath(".//tr[1]"));
+        List<WebElement> headingColumns = headingsRow.findElements(By.xpath(".//th"));
+        return headingColumns;
+    }
+
+    public List<List<WebElement>> getRowsWithColumns() {
+        List<WebElement> rows = getRows();
+        List<List<WebElement>> rowsWithColumns = new ArrayList<List<WebElement>>();
+        for (WebElement row: rows){
+            List<WebElement> rowsWihtColumns = row.findElements(By.xpath("./td"));
+            rowsWithColumns.add(rowsWihtColumns);
+        }
+        return rowsWithColumns;
+    }
+
+    public List<Map<String, WebElement>> getRowsWithColumnsByHeadings(){
+        List<List<WebElement>> rowsWithColumns = getRowsWithColumns();
+        List<Map<String, WebElement>> rowsWithColumnsByHeadings = new ArrayList<Map<String, WebElement>>();
+        Map<String, WebElement> rowsByHeadings;
+        List<WebElement> headingsColumns = getHeadings();
+        for (List<WebElement> row: rowsWithColumns){
+            rowsByHeadings = new HashMap<String, WebElement>();
+            for (int i = 0; i < headingsColumns.size(); i++) {
+                String heading = headingsColumns.get(i).getText();
+                WebElement cell = row.get(i);
+                rowsByHeadings.put(heading, cell);
+            }
+            rowsWithColumnsByHeadings.add(rowsByHeadings);
+        }
+        return rowsWithColumnsByHeadings;
+    }
+
+    public String getValueFromCell(int rowNumber, int columnNumber){
+        List<List<WebElement>> rowsWithColumns = getRowsWithColumns();
+        WebElement cell = rowsWithColumns.get(rowNumber - 1).get(columnNumber - 1);
+        return cell.getText();
+    }
+
+    public String getValueFromCell(int rowNumber, String columnName){
+        List<Map<String, WebElement>> rowsWithColumnsByHeadings = getRowsWithColumnsByHeadings();
+        return rowsWithColumnsByHeadings.get(rowNumber - 1).get(columnName).getText();
+    }
+
+}
