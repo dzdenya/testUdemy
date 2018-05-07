e258ae0b3c62eea231809bc45943c81f7b61d875
Denys Zverev <dzdenya@gmail.com>
2018-05-07 09:36:15 +0300

    13. Работа с окнами

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,14 +1,9 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
@@ -22,29 +17,29 @@ public class MainClass {
 
 //        wait = new WebDriverWait(driver, 10);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
 
         driver.get("http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html?ssSourceSiteId=otnpt");
+        String nameWindow = driver.getWindowHandle();
 
-        driver.findElement(By.xpath("//a[text()='jdk-8u171-windows-x64.exe']")).click();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        driver.findElement(By.xpath("//a[text()='Oracle Binary Code License Agreement for Java SE']")).click();
+
+        for( String windowHandle:driver.getWindowHandles()){    //переключаемся в последнее открытое окно
+            driver.switchTo().window(windowHandle);
         }
-        driver.switchTo().alert().accept();
 
-        driver.get("https://google.com");
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
-        executor.executeScript("confirm('Are you sure?');");
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath("//*[@id=\"u02menulink\"]/div[1]"))).build().perform();
+        driver.findElement(By.xpath("//*[@id=\"u02mmenu\"]/div/ul/li[2]/a")).click();
 
-        driver.switchTo().alert().accept();
-        
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.switchTo().window(nameWindow);
+        driver.findElement(By.xpath("//span[text()=\"Overview\"]")).click();
+
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         driver.quit();
     }
 }
