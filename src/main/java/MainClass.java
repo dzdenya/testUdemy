e63ae3c50f61ba9a235f8e7a8a17c5150245bf98
Denys Zverev <dzdenya@gmail.com>
2018-05-07 15:31:51 +0300

    18. Создание скриншотов с помощью Selenium

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,12 +1,13 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
 
-import java.util.List;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
@@ -39,8 +40,16 @@ public class MainClass {
         element.sendKeys(paste);
         element.sendKeys(Keys.ENTER);
 
+        Date dateNow = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("hh_mm_ss");
+        String fileName = format.format(dateNow)+".png";
 
-
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("C:\\Screenshots\\" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         try {
