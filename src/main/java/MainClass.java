dad0f242bee671e71a9892be6daba1837de7746e
Denys Zverev <dzdenya@gmail.com>
2018-05-07 11:56:18 +0300

    16. Проверка наличия элемента

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -25,40 +25,13 @@ public class MainClass {
             System.out.println("Вероятно вы перетягивали окно в момент разворачивания на весь экран");
         }
 
-        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_pushbutton_disabled2");
-        driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
-        WebElement button1 = driver.findElement(By.xpath("//button[@id='myBtn']"));
-        driver.switchTo().defaultContent();
-        WebElement button2 = driver.findElement(By.xpath("//a[@class='w3-button w3-bar-item topnav-icons fa fa-rotate']"));
+        driver.get("https://github.com/");
 
-        driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
-        System.out.println(button1.isDisplayed());
-        driver.switchTo().defaultContent();
-        System.out.println(button2.isEnabled());
+        System.out.println(driver.findElements(By.xpath("//a[text()='Sing in']")).size());
 
-        driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
-        if (button1.isEnabled()) button1.click();
-        driver.switchTo().defaultContent();
-        if (button2.isEnabled()) button2.click();
+        if(driver.findElements(By.xpath("//a[text()='Log in']")).size() > 0) System.out.println("Such element is presented");
+        else System.out.println("Cannot find such element");
 
-///////////////
-        driver.get("http://www.oracle.com/technetwork/java/javase/downloads/jdk10-downloads-4416644.html");
-        WebElement rbutton1 = driver.findElement(By.xpath("//input[@name='agreementjdk-10.0.1-oth-JPR'][1]"));
-        WebElement rbutton2 = driver.findElement(By.xpath("//input[@name='agreementjdk-10.0.1-oth-JPR'][2]"));
-
-        System.out.println(rbutton2.isSelected());
-        if (!rbutton1.isSelected()) rbutton1.click();
-
-/////////////
-        driver.get("https://www.ebay.com/");
-        WebElement link1 = driver.findElement(By.xpath("//a[@title='Electronics - Cameras & Photo']"));
-        WebElement link2 = driver.findElement(By.xpath("//a[text()='Following']"));
-
-        System.out.println(link1.isDisplayed());
-        System.out.println(link2.isDisplayed());
-
-        if (link1.isDisplayed()) link1.click();
-        else link2.click();
 
 
 //        try {
